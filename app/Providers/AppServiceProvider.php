2b6f4c38f65a2f4b111c90b87792950b72f834fb
Simon Dubois <simon@dubandubois.com>
2016-02-23 22:11:39 +0100

    Add blade directives for price & percentage format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->bladeDirectives();
+    }
+
+    /**
+     * Extend Blade with new directives
+     *
+     * @return void
+     */
+    public function bladeDirectives()
+    {
+        // Format percentage with fixed decimal part and symbol
+        Blade::directive('percentage', function ($expression) {
+            return "<?php echo number_format(floatval($expression), 2).' %'; ?>";
+        });
+        // Format price with fixed decimal part and currency
+        Blade::directive('price', function ($expression) {
+            return "<?php echo number_format(floatval($expression), 2).' '.config('core.currency'); ?>";
+        });
     }
 
     /**
