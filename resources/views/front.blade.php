8221b228d0579172c74a924d88c0e76d26a9d52b
Simon Dubois <simon@dubandubois.com>
2016-02-23 22:10:37 +0100

    Update to the latest fontawesome version

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -20,7 +20,7 @@
     <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
 
     <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
 
